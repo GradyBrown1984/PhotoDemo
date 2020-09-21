ddb23b548a592a18dbf9051eb8d206318c75ccff
Grady Brown <grady.brown@outlook.com>
2020-09-21 18:20:10 -0400

    fixed module feature

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //Created by Grady Brown on 9/21/20
-import {photo1, photo2, mattedPhoto1, mattedPhoto2, framedPhoto1, framedPhoto2} from 'photo';
+import {photo1, photo2, mattedPhoto1, mattedPhoto2, framedPhoto1, framedPhoto2} from './photo.js';
 
 console.log(photo1.toString());
 console.log(photo2.toString());
