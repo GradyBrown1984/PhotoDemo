23b40fae3a57ebbf9d769c4ac831a1fb50bdfdc3
Grady Brown <grady.brown@outlook.com>
2020-09-21 18:11:08 -0400

    Convert to using module feature

---
+++ main.js
@@ -0,0 +1,9 @@
+//Created by Grady Brown on 9/21/20
+import {photo1, photo2, mattedPhoto1, mattedPhoto2, framedPhoto1, framedPhoto2} from 'photo';
+
+console.log(photo1.toString());
+console.log(photo2.toString());
+console.log(mattedPhoto1.toString());
+console.log(mattedPhoto2.toString());
+console.log(framedPhoto1.toString());
+console.log(framedPhoto2.toString());
