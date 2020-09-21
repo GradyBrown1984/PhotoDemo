f025f76ac118396957bc34327b28202b349a6f8d
Grady Brown <grady.brown@outlook.com>
2020-09-21 17:28:21 -0400

    Create and Demonstrate Photo Class

--- photo.js
+++ photo.js
@@ -1,1 +1,29 @@
 //Created by Grady Brown on 9/21/20
+
+class Photo{
+    constructor(width = 8, height = 10){
+        this.height = height;
+        this.width = width;
+    }
+
+    price(){
+        if (this.width == 8 && this.height == 10){
+            return 4;
+        } else if (this.width == 10 && this.height == 12){
+            return 6;
+        } else{
+            return 10;
+        }
+        
+    }
+
+    toString(){
+        return 'This is a ' + this.width + ' by ' + this.height + ' photo and it costs ' + this.price();
+    }
+}
+
+let photo1 = new Photo();
+let photo2 = new Photo(10, 12);
+
+console.log(photo1.toString());
+console.log(photo2.toString());
